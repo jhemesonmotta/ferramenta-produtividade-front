01e6376fccaf907f9cc8653ec18a4bb327a35f7f
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-05-20 20:52:46 -0300

    arrumando testes

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { PageSpeedApiService } from 'src/app/services/pagespeed/pagespeed-api.service';
+import { GithubApiService } from 'src/app/services/github/github-api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -7,7 +10,11 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [
+        HttpClientModule
+      ],
+      providers: [GithubApiService, PageSpeedApiService]
     })
     .compileComponents();
   }));
